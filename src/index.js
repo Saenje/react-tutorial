c88969029154c909ae53ddbce6ccde9b09bbe08a
Sean <jetrospect@outlook.com>
2022-04-15 11:07:52 -0400

    pass-data-through-props

--- src/index.js
+++ src/index.js
@@ -6,18 +6,39 @@ const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 class Square extends React.Component {
-    render() {
-      return (
-        <button className="square">
-          {/* TODO */}
-        </button>
-      );
-    }
+  render() {
+    return (
+      <button 
+        className="square" 
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
+      </button>
+    );
   }
+}
   
   class Board extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      }
+    }
+
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
+    }
+
     renderSquare(i) {
-      return <Square />;
+      return (
+        <Square 
+          value={this.state.squares[i]} 
+          onClick={() => this.handleClick(i)}
+        />
+      );
     }
   
     render() {
