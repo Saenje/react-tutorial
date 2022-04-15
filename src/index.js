277e5a63a9a4009002fffd7afbd970a18cde554d
Sean <jetrospect@outlook.com>
2022-04-15 11:27:41 -0400

    defined-click-handler

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ class Square extends React.Component {
       return (
         <button 
           className="square" 
-          onClick={() => {this.setState({value: 'X'})}}
+          onClick={() => {this.props.onClick({value: 'X'})}}
         >
-          {this.state.value}
+          {this.props.value}
         </button>
       );
     }
@@ -24,6 +24,12 @@ class Square extends React.Component {
       this.state = {
         squares: Array(9).fill(null),
       };
+    }
+
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
     }
 
     renderSquare(i) {
